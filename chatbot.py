9347170e7233ba51e71dcc5b5b7d8a1d28aa42b4
manaswikollu <45660079+manaswikollu@users.noreply.github.com>
2018-12-10 14:29:16 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+data = open("conversations.yml",'r').readlines();
+bot.train(data)
+
+print("Chatbot: Hello")
+
+while True:
+    message = input('You:')
+    if message.strip()!= 'Bye':
+          reply=bot.get_response(message)
+          print('ChatBot:',reply)
+    if message.strip()== 'Bye':
+          print('ChatBot: Bye')
+          break
